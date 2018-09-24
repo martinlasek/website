104f525437091f4d3bd0536488c80b924329ebed
Martin Lasek <martinlasek@hotmail.de>
2018-09-24 02:40:28 +0200

    Added imprint page

--- Sources/App/Utilities.swift
+++ Sources/App/Utilities.swift
@@ -2,5 +2,6 @@ enum Route {
     enum Active {
         static let index = ["index": true]
         static let projects = ["projects": true]
+        static let imprint = ["imprint": true]
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,11 @@
 import Vapor
 
 public func routes(_ router: Router) throws {
-    router.get { req in
+    router.get("/") { req in
         return try req.view().render("index", Route.Active.index)
+    }
+
+    router.get("/imprint") { req in
+        return try req.view().render("imprint", Route.Active.imprint)
     }
 }
